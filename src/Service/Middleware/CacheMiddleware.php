10fec6b2fe188c717a7b88061af4a17bd30d8998
ZoDream <648383079@qq.com>
2019-03-30 23:35:27 +0800

    修改

--- src/Service/Middleware/CacheMiddleware.php
+++ src/Service/Middleware/CacheMiddleware.php
@@ -11,9 +11,9 @@ class CacheMiddleware implements MiddlewareInterface {
         if (empty($urls) || !array_key_exists($payload, $urls)) {
             return $next($payload);
         }
-        return cache()->getOrSet(self::class.$payload, function () use ($payload, $next) {
+        return cache()->getOrSet(self::class.$payload.$this->getPath($urls[$payload]), function () use ($payload, $next) {
             return $next($payload);
-        }, $urls[$payload]);
+        }, $urls[$payload]['time']);
     }
 
     private function formatUri($uris) {
@@ -25,8 +25,31 @@ class CacheMiddleware implements MiddlewareInterface {
             if (is_integer($uri)) {
                 list($uri, $time) = [$time, 0];
             }
+            if (!is_array($time)) {
+                $time = compact('time');
+            }
+            if (!isset($time['params'])) {
+                $time['params'] = [];
+            }
             $data[trim($uri, '/')] = $time;
         }
         return $data;
+    }
+
+    private function getPath(array $args) {
+        if (!isset($args['params']) || empty($args['params'])) {
+            return '';
+        }
+        if (is_callable($args['params'])) {
+            return call_user_func($args['params']);
+        }
+        $data = [];
+        foreach ((array)$args['params'] as $item) {
+            if (empty($item)) {
+                continue;
+            }
+            $data[] = sprintf('%s=%s', $item, app('request')->get($item));
+        }
+        return implode('-', $data);
     }
 }
