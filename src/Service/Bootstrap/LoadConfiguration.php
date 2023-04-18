d5b5eb73eccb4a6d9234cd3b22f6f260c503c54f
ZoDream <648383079@qq.com>
2023-04-18 17:51:09 +0800

    update config

--- src/Service/Bootstrap/LoadConfiguration.php
+++ src/Service/Bootstrap/LoadConfiguration.php
@@ -11,19 +11,13 @@ class LoadConfiguration {
     public function bootstrap(Application $app)
     {
         $items = [];
-
-        // First we will see if we have a cache configuration file. If we do, we'll load
-        // the configuration items from that file so that it is very quick. Otherwise
-        // we will need to spin through every configuration file and load them all.
         if (is_file($cached = $this->getCachedConfigPath())) {
             $items = require $cached;
 
             $loadedFromCache = true;
         }
-
-        // Next we will spin through all of the configuration files in the configuration
-        // directory and load each one into the repository. This will make all of the
-        // options available to the developer for use in various parts of this app.
+        $app->singleton(Repository::class, SystemConfig::class);
+        $app->alias('config', Repository::class);
         $app->instance('config', $config = new SystemConfig($items));
 
         if (! isset($loadedFromCache)) {
